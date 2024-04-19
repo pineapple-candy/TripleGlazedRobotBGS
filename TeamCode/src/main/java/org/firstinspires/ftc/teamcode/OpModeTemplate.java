ce60e67094f834d7540fbcedafbc273b55658bb9
Sebmm116 <160821324+Sebmm116@users.noreply.github.com>
2024-04-20 09:34:21 +1000

    Create OpModeTemplate.java
    Robot controller for FTC off-season.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTemplate.java
@@ -0,0 +1,96 @@
+/* Copyright (c) 2021 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name="OpModeExampleClass", group="Linear OpMode")
+public class OpModeTemplate extends LinearOpMode {
+    //Motor Setup Pt.1
+    private DcMotor         backLeft  = null;
+    private DcMotor         backRight   = null;
+    private ElapsedTime     runtime = new ElapsedTime();
+
+
+    static final double     FORWARD_SPEED = 0.6;
+    static final double     TURN_SPEED    = 0.5;
+    private DcMotor         frontLeft  = null;
+    private DcMotor         frontRight   = null;
+
+
+
+    @Override
+    public void runOpMode() {
+        //Motor Setup Pt.2
+        backLeft  = hardwareMap.get(DcMotor.class, "BackLeft");
+        backRight = hardwareMap.get(DcMotor.class, "BackRight");
+        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        while(opModeIsActive())   {
+            //Calculate motor speed based off controller input
+            double drive = gamepad1.left_stick_y/2;
+            double turn = gamepad1.right_stick_x/2;
+
+            frontLeft.setPower(drive - turn);
+            backLeft.setPower(drive - turn);
+            frontRight.setPower(drive + turn);
+            backRight.setPower(drive + turn);
+
+
+            if(gamepad1.a) {
+                GoForwardThreeSeconds();
+            }
+            //Set Speed for 3 seconds
+
+
+            //set motorspeed
+
+        }
+
+        GoForwardThreeSeconds();
+    }
+    public void GoForwardThreeSeconds() {
+        runtime.reset();
+        while(runtime.seconds() < 3) {
+            frontLeft.setPower(1);
+            frontRight.setPower(1);
+            backLeft.setPower(1);
+            backRight.setPower(1);
+        }
+    }
+}
