dcb54f614bcabfb0f607f237168d68fe0b58c4d9
Sebmm116 <160821324+Sebmm116@users.noreply.github.com>
2024-04-30 16:56:11 +1000

    SUMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMARY

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StateNode.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+public class StateNode {
+    public static void enter();
+    public static void tick();
+    public static void exit();
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Why.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+public class Why extends StateNode {
+    public static void enter(){
+
+    }
+    public static void tick(){
+
+    }
+    public static void exit(){
+
+    }
+
+}
