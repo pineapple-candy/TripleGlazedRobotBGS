5a88f5702d075bdac6edeb49e4ba0a47e158d7f2
Sebmm116 <160821324+Sebmm116@users.noreply.github.com>
2024-07-17 08:05:50 +1000

    Create TakeAnInputAndGiveADifferentOutput.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TakeAnInputAndGiveADifferentOutput.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+//import help.me
+
+public class TakeAnInputAndGiveADifferentOutput {
+    //Now, where to start?
+    //Let's see, we'll get the input first, and then process it.
+    //...But where from?
+    //...I know! the terminal!
+    //That's a little too hard, (wasn't first result of google search) let's try something else.
+    //An integer!!! For this purpose we'll set the integer to 13 (an unlucky number).
+    private int Suffering = 13;
+
+}
