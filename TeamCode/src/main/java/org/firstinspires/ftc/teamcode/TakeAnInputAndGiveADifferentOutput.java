ba3c7b393bda5a77ef2eb239d9e8384ff3098488
Sebmm116 <160821324+Sebmm116@users.noreply.github.com>
2024-07-18 11:49:25 +1000

    Suffered More
    What am I doing with my life?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TakeAnInputAndGiveADifferentOutput.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TakeAnInputAndGiveADifferentOutput.java
@@ -8,6 +8,21 @@ public class TakeAnInputAndGiveADifferentOutput {
     //...I know! the terminal!
     //That's a little too hard, (wasn't first result of google search) let's try something else.
     //An integer!!! For this purpose we'll set the integer to 13 (an unlucky number).
-    private int Suffering = 13;
+    static int Suffering = 0;
+
+    private static void getALife(int headaches, int childhoodTraumas, int optimism) {
+        Suffering += 0.25 * headaches;
+        Suffering += childhoodTraumas;
+        Suffering /= optimism;
+    }
+
+    private static void happyGuy() {
+        getALife(4,1,5);
+
+        System.out.println("Suffering: " + Suffering);
+    }
+
+
+
 
 }
